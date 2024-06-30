2a6f053a6fd947ea2935a29446d8290365be1dfc
sfxoozore <oozore@seamfix.com>
2024-06-30 11:54:18 +0100

    updated folder strucuture

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from './db/DatabaseModule';
+import { DatabaseModule } from './libs/db/DatabaseModule';
 
 @Module({
   imports: [
